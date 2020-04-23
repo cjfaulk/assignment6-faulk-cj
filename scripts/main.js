b7aec1b412524bd0db79dc49c999696aebe5ad4a
CJ Faulk <cjfaulk@ufl.edu>
2020-04-23 13:59:04 -0400

    Fixed js elements on index.html

---
+++ scripts/main.js
@@ -0,0 +1,22 @@
+var members = [
+  'Dave Grohl',
+  'Nate Mendel',
+  'Pat Smear',
+  'Taylor Hawkins',
+  'Rami Jaffee',
+  'Chris Shiflett'
+]
+
+var tour = {
+  chicago: "July 29th and 30th",
+  seattle: "September 1st",
+  edmonton: "September 4th",
+}
+
+var x = 0;
+
+while (x < 5) {
+  console.log("Times running through the loop: " + x);
+
+  x++;
+}
